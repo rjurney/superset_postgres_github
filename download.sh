3af1eae3799c57aa4ff213d81a45c9181a881c40
Russell Jurney <russell.jurney@gmail.com>
2019-04-19 10:43:57 -0700

    Script that downloads one year of Github Archive data (4/1/18-3/31/19)

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Fetch one year of Github Archive events, starting in March 2018 and going to March 2019
+wget -P data/ http://data.gharchive.org/2018-{04..12}-{01..31}-{0..23}.json.gz
+wget -P data/ http://data.gharchive.org/2019-{01..03}-{01..31}-{0..23}.json.gz
