2703de03b4b2e0f8b884bbc53e7b24f1d058216a
Russell Jurney <russell.jurney@gmail.com>
2019-06-11 18:01:14 -0700

    starter

--- download.sh
+++ download.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # Fetch one year of Github Archive events, starting in March 2018 and going to March 2019
-wget -P data/ http://data.gharchive.org/2018-{04..12}-{01..31}-{0..23}.json.gz
+wget -P data/ http://data.gharchive.org/2018-{04..08}-{01..31}-{0..23}.json.gz
+wget -P data/ http://data.gharchive.org/2018-{09..12}-{01..31}-{0..23}.json.gz
 wget -P data/ http://data.gharchive.org/2019-{01..03}-{01..31}-{0..23}.json.gz
---
+++ emr_bootstrap.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo pip install --upgrade pip
+sudo pip install frozendict findspark numpy pandas
