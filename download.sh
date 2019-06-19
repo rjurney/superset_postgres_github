a6e888dae909a7ad51754f2186f18080b12bb200
Russell Jurney <russell.jurney@gmail.com>
2019-06-19 13:32:54 -0700

    Get all data from 2014 to end of 2019

--- download.sh
+++ download.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-# Fetch one year of Github Archive events, starting in March 2018 and going to March 2019
-wget -P data/ http://data.gharchive.org/2018-{04..08}-{01..31}-{0..23}.json.gz
-wget -P data/ http://data.gharchive.org/2018-{09..12}-{01..31}-{0..23}.json.gz
-wget -P data/ http://data.gharchive.org/2019-{01..03}-{01..31}-{0..23}.json.gz
+# Fetch one year of Github Archive events, starting in  and going to June 2019
+wget -nc -P data/ http://data.gharchive.org/2011-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2012-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2013-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2014-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2015-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2016-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2017-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2018-{01..12}-{01..31}-{0..23}.json.gz
+wget -nc -P data/ http://data.gharchive.org/2019-{01..06}-{01..31}-{0..23}.json.gz
